364bf18d4ef4cb79359ea4a2dc93955bf227f2f3
Ashmita Kunwar <ashmita.kunwar@deerwalk.edu.np>
2016-05-18 21:49:05 -0700

    Domain and controller added

--- grails-app/controllers/dwitnewsarticlelogsheet/ReporterController.groovy
+++ grails-app/controllers/dwitnewsarticlelogsheet/ReporterController.groovy
@@ -2,9 +2,16 @@ package dwitnewsarticlelogsheet
 
 class ReporterController {
     //static scaffold=true;
+    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
 
     def index() { }
     def create(){
-        render view:create;
+        render view:'create';
+       // [reporterInstance: new Reporter(params)]
+    }
+    def save(){
+        def reporter=new Reporter(params)
+        reporter.save();
+        render "Reporter are saved successfully"
     }
 }
---
+++ grails-app/views/reporter/edit.gsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: asmita
+  Date: 5/18/2016
+  Time: 9:48 PM
+--%>
+
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title></title>
+</head>
+
+<body>
+
+</body>
+</html>
